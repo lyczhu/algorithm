70070758e7111f97179ae9d980f1d2eb1a59c632
yushing <luoyushing@gmail.com>
2019-11-29 18:09:51 +0800

    添加单链表操作

--- src/main/java/algo/link/SingleLinkedList.java
+++ src/main/java/algo/link/SingleLinkedList.java
@@ -2,6 +2,7 @@ package algo.link;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * TODO: describe please.
@@ -11,18 +12,87 @@ import lombok.RequiredArgsConstructor;
  */
 @Data
 public class SingleLinkedList {
+
+    /**
+     * 1. 删除指定节点的下一节点
+     * 2. 删除指定节点
+     * 3. 删除头节点
+     * 4. 删除尾节点
+     * 5. 头部插入
+     * 6. 尾部插入
+     * 7. 指定节点后插入
+     * 8. 创建链表
+     * 9. 按值查找
+     * 10. 按位置查找
+     * 11. 链表反转
+     */
+
+
     private Node head;
 
-    public SingleLinkedList(int value) {
-        head = new Node(value);
+    /**
+     * 尾部插入（顺序插入）
+     * @param value
+     */
+    public void add(int value) {
+        Node node = new Node(value);
+        addNode(node);
     }
 
-    public void add(Node node) {
-        head.next = node;
+    /**
+     * 尾部插入（顺序插入）
+     * @param node
+     */
+    public void addNode(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            Node n = head;
+            if (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+
+    /**
+     * 头部插入
+     * @param value
+     */
+    public void addAtHead(int value) {
+        Node node = new Node(value);
+        addAtHead(node);
+    }
+
+    /**
+     * 头部插入
+     * @param node
+     */
+    public void addAtHead(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    /**
+     * 按值查找节点
+     * @param value
+     * @return
+     */
+    public Node findByValue(int value) {
+        Node n = head;
+        if (head != null && value != head.data) {
+            n = n.next;
+        }
+        return n;
     }
 
     class Node {
         private int data;
+        @Setter
         private Node next;
 
         Node(int data) {
@@ -39,7 +109,7 @@ public class SingleLinkedList {
     }
 
     public static void main(String[] args) {
-        SingleLinkedList sll = new SingleLinkedList(1);
+        SingleLinkedList sll = new SingleLinkedList();
         System.out.println(sll);
     }
 }
