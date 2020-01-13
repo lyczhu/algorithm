7b31c174fa88b0aed4edb9835349d8c69564e92e
yushing <luoyushing@gmail.com>
2020-01-13 17:50:14 +0800

    ugly number

---
+++ src/test/java/algo/leetcode/simple/UglyNumberTest.java
@@ -0,0 +1,12 @@
+package algo.leetcode.simple;
+
+import org.junit.jupiter.api.Test;
+
+class UglyNumberTest {
+
+	@Test
+	void isUgly() {
+		UglyNumber un = new UglyNumber();
+		System.out.println(un.isUgly(6));
+	}
+}
